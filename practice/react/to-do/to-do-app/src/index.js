a427a8f68d44d470a9439449404eecc7508a5353
Andrey Strelkovskiy <a.strelkovskiy@sana-commerce.com>
2020-05-22 11:21:21 +0300

    Add react to-do app

--- practice/react/to-do/to-do-app/src/index.js
+++ practice/react/to-do/to-do-app/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './behavior/rootReducer';
+
+const preloadedState = { list: [{ id: 0, tesxt: 'Hello world', completed: false }] };
+const store = createStore(reducer, preloadedState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
